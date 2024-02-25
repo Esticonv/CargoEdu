d5f624de5094b2ad545045183458c5a380abd456
Borisov Dmitriy <incoming_for_me@mail.ru>
2024-02-25 20:34:52 +0500

    Add Domain and EF

---
+++ MachineManagerService/Domain/Domain.Entities/Machine.cs
@@ -0,0 +1,11 @@
+﻿namespace MachineManagerService.Domain.Entities
+{
+    public class Machine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double CargoCapacity { get; set; }
+        public double MaxSpeed {  get; set; }
+
+    }
+}
---
+++ MachineManagerService/Infrastructure/Infrastructure.EntityFramework/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EntityFramework
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MachineManagerService.Domain.Entities.Machine> Machines { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            /*modelBuilder.Entity<Course>()
+                .HasMany(u => u.Lessons)
+                .WithOne(c => c.Course)
+                .IsRequired();*/
+        }
+    }
+}
