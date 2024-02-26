8364d937140c2b35141adb5f3c1ec17246427b0e
Borisov Dmitriy <incoming_for_me@mail.ru>
2024-02-26 21:20:16 +0500

    Create DB scheme and Entities

---
+++ src/MachineManagerService/Domain/Domain.Entities/Cargo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineManagerService.Domain.Entities
+{
+    public class Cargo
+    {
+        public int CargoId { get; set; }
+        public string Destination { get; set; }
+        public double Volume { get; set; }
+        public double Weight { get; set; }
+    }
+}
---
+++ src/MachineManagerService/Domain/Domain.Entities/Machine.cs
@@ -0,0 +1,16 @@
+﻿namespace MachineManagerService.Domain.Entities
+{
+    public class Machine
+    {
+        public int MachineId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double VolumeCapacity { get; set; }
+        public double WeightCapacity { get; set; }
+        public double MaxSpeed {  get; set; }
+        public decimal CostPerDistance { get; set; }
+        public int CurrentTaskId { get; set; }
+        public MachineTask CurrentTask { get; set; }
+        public List<Cargo> Сargoes { get; set; }
+
+    }
+}
