829c49811d5aaebfd9236f30a19a532aa7ffc40e
Borisov Dmitriy <incoming_for_me@mail.ru>
2024-03-26 19:41:22 +0500

    Add auto mapping with Mapperly

--- src/MachineManagerService/Application/Application.Application/Implementations/MachineService.cs
+++ src/MachineManagerService/Application/Application.Application/Implementations/MachineService.cs
@@ -1,6 +1,7 @@
 ﻿using R7P.MachineManagerService.Application.Models;
 using R7P.MachineManagerService.Application.Interfaces;
 using R7P.MachineManagerService.Domain.Entities;
+using R7P.MachineManagerService.Application.Interfaces.Mapping;
 
 
 namespace R7P.MachineManagerService.Application.Implementations
@@ -16,14 +17,7 @@ namespace R7P.MachineManagerService.Application.Implementations
             var results=new List<MachineDto>();
             
             foreach(var machine in machines.Take(5)) {
-                results.Add(new MachineDto {
-                    Id = machine.Id,
-                    Name = machine.Name,
-                    VolumeCapacity = machine.VolumeCapacity,
-                    WeightCapacity = machine.WeightCapacity,
-                    MaxSpeed = machine.MaxSpeed,
-                    CostPerDistance = machine.CostPerDistance,
-                });
+                results.Add(MachineMapper.ToDto(machine));
             }
             return results;
         }
@@ -33,26 +27,13 @@ namespace R7P.MachineManagerService.Application.Implementations
             var machine = await _machineRepository.GetAsync(id)
                 ?? throw new InvalidOperationException($"Не найден адрес с идентифкатором {id}");
 
-            return new MachineDto {
-                Id = machine.Id,
-                Name = machine.Name,
-                VolumeCapacity = machine.VolumeCapacity,
-                WeightCapacity = machine.WeightCapacity,
-                MaxSpeed = machine.MaxSpeed,
-                CostPerDistance = machine.CostPerDistance,
-            };
+            return MachineMapper.ToDto(machine);
         }
 
         public async Task<MachineDto> Add(MachineDto machine)
         {
-            var machineEntity = new Machine {
-                Name = machine.Name,
-                VolumeCapacity = machine.VolumeCapacity,
-                WeightCapacity = machine.WeightCapacity,
-                MaxSpeed = machine.MaxSpeed,
-                CostPerDistance = machine.CostPerDistance,
-                Tasks = [new MachineTask()]
-            };
+            var machineEntity = MachineMapper.ToDomain(machine);
+            machineEntity.Tasks ??= [new MachineTask()];
 
             _machineRepository.Add(machineEntity);
             await _machineRepository.SaveChangesAsync();
---
+++ src/MachineManagerService/Application/R7P.MachineManagerService.Application.Interfaces/Mapping/MachineMapper.cs
@@ -0,0 +1,11 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace R7P.MachineManagerService.Application.Interfaces.Mapping
+{
+    [Mapper]
+    public static partial class MachineMapper
+    {
+        public static partial Models.MachineDto ToDto(Domain.Entities.Machine machine);
+        public static partial Domain.Entities.Machine ToDomain(Models.MachineDto machineDto);
+    }
+}
