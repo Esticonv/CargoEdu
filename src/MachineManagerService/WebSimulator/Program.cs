0e9c098e9369a446d22d0a11b6ac06895e5276b2
Borisov Dmitriy <incoming_for_me@mail.ru>
2024-04-12 20:31:14 +0500

    WIP Simulator (Blazor WebApp)

---
+++ src/MachineManagerService/WebSimulator/Program.cs
@@ -0,0 +1,37 @@
+using R7P.MachineManagerService.WebSimulator.Components;
+
+namespace R7P.MachineManagerService.WebSimulator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            string connectionString = builder.Configuration.GetSection("ConnectionStrings")["DefaultConnection"] 
+                ?? throw new InvalidOperationException("Not found connection string");
+
+            builder.Services.AddHttpClient("WebAPI", client =>
+                client.BaseAddress = new Uri(connectionString));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
