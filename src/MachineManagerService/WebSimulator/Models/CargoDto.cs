24d33f6a31fb63be6795a03dad3d8d5c1d3d461a
Borisov Dmitriy <incoming_for_me@mail.ru>
2024-04-14 17:21:30 +0500

    WIP MachineManagerService.WebSimulator

---
+++ src/MachineManagerService/WebSimulator/Models/CargoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace R7P.MachineManagerService.WebSimulator.Models
+{
+    public class CargoDto
+    {
+        public string? Destination { get; set; }
+        public double Volume { get; set; }
+        public double Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"CargoDto [Destination: {Destination}, Volume: {Volume}, Weight: {Weight}]";
+        }
+    }
+}
